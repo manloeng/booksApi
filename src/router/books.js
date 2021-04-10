21aa1bdc1543771d1e1f4c9842327d0d2971ac12
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-10 12:13:25 +0100

    Fix: Error

--- src/router/books.js
+++ src/router/books.js
@@ -1,5 +1,5 @@
 const booksRouter = require("express").Router();
-const { getBooks } = require("../controller/books/getBooks");
+const getBooks = require("../controller/books/getBooks");
 const { methodNotAllowed } = require("../error");
 
 booksRouter.route("/").get(getBooks).all(methodNotAllowed);
