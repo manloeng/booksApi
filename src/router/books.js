0f446c939f574a55d4ff72141aaa83913c1ed8c9
Andrew <37455284+manloeng@users.noreply.github.com>
2021-04-10 11:55:25 +0100

    Add Get books route

---
+++ src/router/books.js
@@ -0,0 +1,7 @@
+const booksRouter = require("express").Router();
+const { getBooks } = require("../controller/books/getBooks");
+const { methodNotAllowed } = require("../error");
+
+booksRouter.route("/").get(getBooks).all(methodNotAllowed);
+
+module.exports = booksRouter;
